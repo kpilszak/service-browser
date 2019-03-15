d0f7f54e674ea545078e01eb04852c46fc39a76e
kpilszak <klaudia.pilszak@gmail.com>
2019-03-15 18:11:43 +0100

    Add actual RMI remote service

--- src/ServiceServer.java
+++ src/ServiceServer.java
@@ -2,6 +2,6 @@ import java.rmi.*;
 
 public interface ServiceServer extends Remote {
 
-    Object[] getServiceList[] throws RemoteException;
+    Object[] getServiceList() throws RemoteException;
     Service getService(Object serviceKey) throws RemoteException;
 }
---
+++ src/ServiceServerImpl.java
@@ -0,0 +1,43 @@
+import java.rmi.*
+import java.rmi.server.*;
+import java.util.HashMap;
+
+public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer {
+
+    HashMap serviceList;
+
+
+    public ServiceServerImpl() throws RemoteException {
+        setUpServices();
+    }
+
+
+    private void setUpServices() {
+        serviceList = new HashMap();
+        serviceList.put("Dice Rolling Service", new DiceService());
+        serviceList.put("Day of the Week Service", new DayOfTheWeekService());
+    }
+
+
+    @Override
+    public Object[] getServiceList() throws RemoteException {
+        System.out.println("in remote");
+        return serviceList.keySet().toArray();
+    }
+
+    @Override
+    public Service getService(Object serviceKey) throws RemoteException {
+        Service theService = (Service) serviceList.get(serviceKey);
+        return theService;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Naming.rebind("ServiceServer", new ServiceServerImpl());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Remote service is running");
+    }
+}
